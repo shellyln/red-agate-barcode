018d420f9b08ae52daeb2795cce0fa2bda574e53
shellyln <shellyln@users.noreply.github.com>
2017-12-11 22:08:07 +0900

    Add JapanPostal

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,9 @@ export * from './barcode/BarcodeBase';
 
 export * from './barcode/Code39';
 export * from './barcode/Code128';
+export * from './barcode/Ean';
 export * from './barcode/Itf';
+export * from './barcode/JapanPostal';
 export * from './barcode/Nw7';
 
 export * from './barcode/Qr';
